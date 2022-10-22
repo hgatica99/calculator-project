d64ad1bd9215876917499b7a9471d6330633e0e7
Hector Gatica <hectorgatica99@gmail.com>
2022-10-22 10:18:27 -0400

    Working Code v1

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -12,25 +12,49 @@ export class Calculator {
     }
 
     // determine the current operation's label
-    getAction() {
-    }
+    getAction() {}
 
     // performa a calculation based on the currently selected operation
-    operate() {}
-
-    attemptedDivideByZero = false;
+    operate() {
+        switch (this.operator){
+            case 'add': this.add();
+            break;
+            case 'subtract': this.subtract();
+            break;
+            case 'multiply': this.multiply();
+            break;
+            case 'divide': this.divide();
+            break;
+        }
+    }
 
     // perform addition operation
-    add() {}
-
+    add() {
+        console.log('addition')
+        this.value = this.firstNumber + this.secondNumber
+    }
+    
     // perform subtraction operation
-    subtract() {}
+    subtract() {
+        console.log('subtraction')
+        this.value = this.firstNumber - this.secondNumber
+    }
 
     // perform multiplication operation
-    multiply() {}
+    multiply() {
+        console.log('multiplication')
+        this.value = this.firstNumber * this.secondNumber
+    }
 
     // perform divide operation
-    divide() {}
+    divide() {
+        console.log('division')
+        if (this.secondNumber == 0){
+            console.log("Can't divide by 0")
+        } else {
+        this.value = this.firstNumber/this.secondNumber
+        }
+    }
 }
 
 //DO NOT TOUCH THIS LINE OF CODE//
--- calculator/controller.js
+++ calculator/controller.js
@@ -1,17 +1,42 @@
-import { Calculator } from "./calculator";
+import { Calculator } from "./calculator.js";
 
-// setup variables and get a calculator instance
-function CreateCalculator() {
-    let firstNumber = document.getElementById("firstNum").value;
-    let secondNumber = document.getElementById("secondNum").value;
-    let operator = document.querySelector('input[name="asdm":checked]').id;
-    let value = 0
+// Setup variables and get a calculator instance
+let equalBtn = document.getElementById('equalBtn');
+let clearBtn = document.getElementById('clearBtn');
+let firstNum = ''
+let secondNum = ''
+let operator = ''
+const myCalculator = new Calculator();
 
-    const myCalculator = Calculator()
+equalBtn.addEventListener("click", calculate);
+clearBtn.addEventListener("click", clearValues);
+
+// function CreateCalculator() {}
+
+// Checks if both numbers are numbers.
+function checkIfNumber(x, y){
+    if (isNaN(x) === false && isNaN(y) === false){
+        return true
+    } else {
+        return false
+    }
 }
-// perform a calculation when the operator button is clicked
+
+// Perform a calculation when the operator button is clicked
 function calculate() {
-    myCalculator.firstNumber;
+    firstNum = document.getElementById('firstNum').value
+    secondNum = document.getElementById('secondNum').value
+    operator = getOperator();
+    // Check if both input fields are numbers
+    if(checkIfNumber(firstNum, secondNum)){
+        myCalculator.firstNumber = Number(firstNum)
+        myCalculator.secondNumber = Number(secondNum)
+        myCalculator.operator = operator
+        myCalculator.operate()
+        console.log(myCalculator.value)
+    } else {
+        console.log("Please enter only numbers")
+    }
 }
 
 /**
@@ -21,13 +46,18 @@ function calculate() {
 function updateResultText(value) {}
 
 // should clear input text values and focus the first number input
-function clearValues() {}
+function clearValues() {
+    document.getElementById('firstNum').value = ' ';
+    document.getElementById('secondNum').value = ' ';
+    document.getElementById('add').checked = true;
+}
 
 /**
  * get the selected operator from the UI
  * @returns Operators
  */
+
 function getOperator() {
-    let checkedBtn = document.querySelector('input[name="asdm":checked]').id;
+    let checkedBtn = document.querySelector('input[name="asdm"]:checked').value;
     return checkedBtn
 }
